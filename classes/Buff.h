51f4eeb29a4b319f2d55e12d25a791302dd1fc08
afloresmtz <a01708363@tec.mx>
2024-11-22 12:17:41 -0600

    Added Slime and Skeleton attributes and respective constructors

--- classes/Buff.h
+++ classes/Buff.h
@@ -8,12 +8,16 @@ private:
     int intensity;
     Character targetCharacter;
     Enemy targetEnemy;
+    Slime targetEnemySlime;
+    Skeleton targetEnemySkeleton;
     string targetStat;
     int duration;
 public:
     Buff(Character targetCharacter, string targetStat, int intensity);
     Buff(Character targetCharacter, string targetStat, int intensity, int duration);
     Buff(Enemy targetEnemy, string targetStat, int intensity, int duration);
+    Buff(Slime targetEnemySlime, string targetStat, int intensity, int duration);
+    Buff(Skeleton targetEnemySkeleton, string targetStat, int intensity, int duration);
 
     void affectCharacterStat();
     void affectEnemyStat();
