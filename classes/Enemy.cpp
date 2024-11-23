1e28d1a37d855272e68fcaad615fbafd37e5c095
afloresmtz <a01708363@tec.mx>
2024-11-22 23:02:55 -0600

    Added "name" attribute and reworked "attack" method

--- classes/Enemy.cpp
+++ classes/Enemy.cpp
@@ -1,11 +1,20 @@
 #include "Enemy.h"
 
-Enemy::Enemy(int health, int strength, int magicRes, int meleeRes) {
+#include <string>
+#include "Character.h"
+#include "Buff.h"
+
+Enemy::Enemy(const std::string& name, int health, int strength, int magicRes, int meleeRes) {
+    this->name = name;
     this->health = health;
     maxHealth = health;
     this->strength = strength;
     this->magicRes = magicRes;
     this->meleeRes = meleeRes;
+}
+
+const std::string Enemy::getName() {
+    return name;
 }
 
 int Enemy::getHealth() {
@@ -36,8 +45,17 @@ void Enemy::setMeleeRes(int amount) {
     meleeRes = amount;
 }
 
-void Enemy::attack(Character target) {
-    target.setHealth(target.getHealth() - (strength - target.getDefense()));
+void Enemy::attack(Character* target) {
+    if(strength - target->getDefense() < 0) {
+        cout << "The enemy did 0 damage to the " << target->getName() << endl;
+    }
+    else {
+        target->setHealth(target->getHealth() - (strength - target->getDefense()));
+        cout << "The enemy did " << to_string(strength - target->getDefense()) << " damage to the " << target->getName() << endl;
+        if(target->getHealth() - (strength - target->getDefense()) < 0) {
+            target->setHealth(0);
+        }
+    }
 }
 void Enemy::applyBuff(Buff current_buff) {
     current_buff.affectEnemyStat();
