e4be88ed164ec59010560b3f1aac1e9ee49ea01f
afloresmtz <a01708363@tec.mx>
2024-11-23 16:10:06 -0600

    Added Comments

--- classes/Buff.h
+++ classes/Buff.h
@@ -7,17 +7,21 @@ class Enemy;
 
 class Buff {
 private:
+    // priuvate attributes
     int intensity;
 
     std::string targetStat;
 
+    // composition attributes
     Character* targetCharacter;
     Enemy* targetEnemy;
     
 public:
+    // constructors
     Buff(Character* targetCharacter, const std::string& targetStat, int intensity);
     Buff(Enemy* targetEnemy, const std::string& targetStat, int intensity);
 
+    // methods
     void affectCharacterStat();
     void affectEnemyStat();
 };
