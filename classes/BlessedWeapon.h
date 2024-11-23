2c7ea1ca0f7513e006023abe38005871c81c3afd
afloresmtz <a01708363@tec.mx>
2024-11-23 16:18:24 -0600

    Added Comments

--- classes/BlessedWeapon.h
+++ classes/BlessedWeapon.h
@@ -3,13 +3,16 @@
 
 #include "Weapon.h"
 
-class BlessedWeapon : public Weapon {
+class BlessedWeapon : public Weapon {  // inheritance from Weapon
 private:
+    // private subclass attributes
     std::string blessing;
     int intensity;
 public:
+    // constructor
     BlessedWeapon(std::string name, int damage, bool type, std::string blessing, int intensity);
 
+    // getter for subclass attributes
     std::string getBlessing();
     int getIntensity();
 };
