7a5c334c4dadd40d435cef718a9a15d06f6d21d7
afloresmtz <a01708363@tec.mx>
2024-11-23 12:15:47 -0600

    Updated Split Method

--- classes/Slime.h
+++ classes/Slime.h
@@ -2,13 +2,14 @@
 #define SLIME_H
 
 #include "Enemy.h"
+#include "Character.h"
 
 class Slime : public Enemy {
 // no private attributes, it shares Enemy's protected attributes
 public:
     Slime(int health, Stat strength);
 
-    void split(int pieces);
+    void scramble();
 };
 
 #endif
