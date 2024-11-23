40861f456d61b4281feabc2108e7fc19655cdd86
afloresmtz <a01708363@tec.mx>
2024-11-23 16:13:55 -0600

    Added Comments

--- classes/Enemy.h
+++ classes/Enemy.h
@@ -8,16 +8,20 @@
 
 class Enemy {
 private:
+    // private attributes
     std::string name;
     int health;
     int maxHealth;
     int strength;
     int magicRes;
     int meleeRes;
+    // composition attribute
     Weapon* equipped_weapon;
 public:
+    // constructor
     Enemy(const std::string& name, int health, int strength, int magicRes, int meleeRes, Weapon* equipped_weapon);
-
+    
+    // getters and setters for all stats
     const std::string getName();
     Weapon getWeapon();
 
@@ -34,7 +38,8 @@ public:
 
     int getMeleeRes();
     void setMeleeRes(int amount);
-
+    
+    // action method
     void attack(Character* target);
 };
 
