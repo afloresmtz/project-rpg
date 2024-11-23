b09ce5aae3ae54056cd44cf7842767bcc3888ce3
afloresmtz <a01708363@tec.mx>
2024-11-22 18:08:33 -0600

    Changed Switch to If/Elif Statements
    Apparently C++ does not allow strings as the main variable of the switch.

--- classes/Buff.cpp
+++ classes/Buff.cpp
@@ -23,51 +23,57 @@ Buff::Buff(Enemy targetEnemy, string targetStat, int intensity, int duration) {
 }
 
 void Buff::affectCharacterStat() {
-    switch(targetStat) {
-        case "health":
-            if(targetCharacter.getHealth() + intensity <= targetCharacter.getMaxHealth()) {
-                targetCharacter.setHealth(targetCharacter.getHealth() + intensity);
-            }
-            else {
-                cout << "Health is already at max." << endl;
-            }
-        case "mana":
-            if(targetCharacter.getMana() + intensity <= targetCharacter.getMaxmana()) {
-                targetCharacter.setMana(targetCharacter.getMana() + intensity);
-            }
-            else {
-                cout << "mana is already at max." << endl;
-            }
-        case "maxHealth":
-            targetCharacter.setMaxHealth(targetCharacter.getMaxHealth() + intensity);
-        case "defense":
-            targetCharacter.setDefense(targetCharacter.getDefense() + intensity);
-        case "meleeDmg":
-            targetCharacter.setMeleeDmg(targetCharacter.getMeleeDmg() + intenisty);
-        case "magicDmg":
-            targetCharacter.setMagicDmg(targetCharacter.getMagicDmg() + intensity);
-        case "maxMana":
-            targetCharacter.setMaxMana(targetCharacter.getMaxMana() + intensity);
-        case "critChance":
-            targetCharacter.setCritChance(targetCharacter.getCritChance() + intensity);
-        case "critDmg":
-            targetCharacter.setCritDmg(targetCharacter.getCritDmg() + intensity);
-
+    if (targetStat == "health") {
+        if (targetCharacter.getHealth() + intensity <= targetCharacter.getMaxHealth()) {
+            targetCharacter.setHealth(targetCharacter.getHealth() + intensity);
+        } else {
+            cout << "Health is already at max." << endl;
+        }
+    } 
+    else if (targetStat == "mana") {
+        if (targetCharacter.getMana() + intensity <= targetCharacter.getMaxMana()) {
+            targetCharacter.setMana(targetCharacter.getMana() + intensity);
+        } else {
+            cout << "Mana is already at max." << endl;
+        }
+    } 
+    else if (targetStat == "maxHealth") {
+        targetCharacter.setMaxHealth(targetCharacter.getMaxHealth() + intensity);
+    } 
+    else if (targetStat == "defense") {
+        targetCharacter.setDefense(targetCharacter.getDefense() + intensity);
+    } 
+    else if (targetStat == "meleeDmg") {
+        targetCharacter.setMeleeDmg(targetCharacter.getMeleeDmg() + intensity);
+    } 
+    else if (targetStat == "magicDmg") {
+        targetCharacter.setMagicDmg(targetCharacter.getMagicDmg() + intensity);
+    } 
+    else if (targetStat == "maxMana") {
+        targetCharacter.setMaxMana(targetCharacter.getMaxMana() + intensity);
+    } 
+    else if (targetStat == "critChance") {
+        targetCharacter.setCritChance(targetCharacter.getCritChance() + intensity);
+    } 
+    else if (targetStat == "critDmg") {
+        targetCharacter.setCritDmg(targetCharacter.getCritDmg() + intensity);
     }
 }
 
 void Buff::affectEnemyStat() {
-    switch(targetStat) {
-        case "health":
-            if(targetEnemy.getHealth() + intensity <= targetEnemy.getMaxHealth()) {
-                targetEnemy.setHealth(targetEnemy.getHealth() + intensity);
-            }
-        case "strength":
-            targetEnemy.setStrength(targetEnemy.getStrength() + intensity);
-        case "magicRes":
-            targetEnemy.setMagicRes(targetEnemy.getMagicRes() + intensity);
-        case "meleeRes":
-            targetEnemy.setMeleeRes(targetEnemy.getMeleeRes() + intensity);
+    if (targetStat == "health") {
+        if (targetEnemy.getHealth() + intensity <= targetEnemy.getMaxHealth()) {
+            targetEnemy.setHealth(targetEnemy.getHealth() + intensity);
+        }
+    } 
+    else if (targetStat == "strength") {
+        targetEnemy.setStrength(targetEnemy.getStrength() + intensity);
+    } 
+    else if (targetStat == "magicRes") {
+        targetEnemy.setMagicRes(targetEnemy.getMagicRes() + intensity);
+    } 
+    else if (targetStat == "meleeRes") {
+        targetEnemy.setMeleeRes(targetEnemy.getMeleeRes() + intensity);
     }
 }
 
